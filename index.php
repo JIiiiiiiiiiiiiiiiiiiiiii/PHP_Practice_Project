eae88ecf1c825ea589672881fe9e96c38fa8f8ba
JIiiiiiiiiiiiiiiiiiiiiii <cjehus@gmail.com>
2025-12-03 15:57:59 +0800

    added functionality for routing mechanisms

--- index.php
+++ index.php
@@ -2,15 +2,4 @@
 
 require 'functions.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/contact') {
-    require 'controllers/contact.php';
-} else {
-    http_response_code(404);
-    echo '404 Not Found';
-}
+require 'router.php';
---
+++ views/404.view.php
@@ -0,0 +1,17 @@
+<?php require __DIR__ . '/partials/head.php'; ?>
+
+    <?php require __DIR__ . '/partials/nav.php'; ?>    
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            <h1 class="2xl font-bold">Sorry, the page you're looking for is not found</h1>
+            
+            <p class="mt-4">It seems like the page has been move.</p>
+            <p>
+                <a href="/" class="text-blue underline">Go back home</a>
+            </p>
+        </div>
+    </main>
+    
+<?php require __DIR__ . '/partials/footer.php'; ?>
