d0fc6042a6e4afe6c16b49d54b1e949ea3a48696
JIiiiiiiiiiiiiiiiiiiiiii <cjehus@gmail.com>
2025-12-08 22:06:04 +0800

    updated db functions && routes handling

---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return 
+[
+    '/' => 'controllers/index.php',
+    '/patients' => 'controllers/patients.php',
+    '/patient' => 'controllers/patient.php',
+    '/patient/create' => 'controllers/patient-create.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+]; 
---
+++ views/403.view.php
@@ -0,0 +1,17 @@
+<?php require __DIR__ . '/partials/head.php'; ?>
+
+<?php require __DIR__ . '/partials/nav.php'; ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        <h1 class="2xl font-bold">Unauthorized Access</h1>
+
+        <p class="mt-4">It seems like you're trying to access something you don't have a permission on.</p>
+        <p>
+            <a href="/" class="text-blue underline">Go back home</a>
+        </p>
+    </div>
+</main>
+
+<?php require __DIR__ . '/partials/footer.php'; ?>
