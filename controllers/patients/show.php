74e4f4f78ff383efa33444232d4de90d3e5de02c
JIiiiiiiiiiiiiiiiiiiiiii <cjehus@gmail.com>
2025-12-14 10:56:22 +0800

    added delete functionality

--- controllers/patients/show.php
+++ controllers/patients/show.php
@@ -7,11 +7,28 @@ $db = new Database($config['database']);
 
 $current_user = 3;
 
-$patient = $db->query('SELECT * FROM patients where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $patient = $db->query('SELECT * FROM patients where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
-authorize($patient['user_id'] === $current_user);
+    authorize($patient['user_id'] === $current_user);
+
+    $db->query('delete from patients where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /patients');
+    exit();
+} else {
+    $patient = $db->query('SELECT * FROM patients where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($patient['user_id'] === $current_user);
+}
+
+
 
 require view('patients/show.view.php', [
     'title' => 'Note',
--- views/patients/show.view.php
+++ views/patients/show.view.php
@@ -8,8 +8,15 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
         <a href="/patients" class="text-blue-500 hover:underline mb-6 inline-block">Go back</a> <br>
-        <p>Name: <?= $patient['name'] ?></p>
-        <p>Age: <?= $patient['age'] ?></p>
+        <div class="">
+            <p>Name: <?= $patient['name'] ?></p>
+            <p>Age: <?= $patient['age'] ?></p>
+        </div>
+
+        <form action="" class="mt-6" method="POST">
+            <input type="hidden" name="id" value="<?= $patient['id'] ?>">
+            <button class="text-sm text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
